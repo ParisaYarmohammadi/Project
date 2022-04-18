ae1465633801c525dd290a37bc089cb11e2b907c
ParisaYarmohammadi <100281553+ParisaYarmohammadi@users.noreply.github.com>
2022-04-18 19:15:02 +0430

    Update BlockableToken.sol

--- BlockableToken.sol
+++ BlockableToken.sol
@@ -5,37 +5,20 @@ import "./StandardToken.sol";
 import '../Ownable.sol';
 
 
-/**
- * @title BlockableToken
- *
- * @dev Simple ERC20 Token example, with blocking (for example, for voting in a DAO contract)
- */
 contract BlockableToken is StandardToken, Ownable {
 
   mapping (address => uint) accountBlocks;
 
-  /**
-   * @dev Block account
-   * @param _account address Account ot block
-   */
   function blockAccount(address _account) onlyOwner {
     var _accountBlocks = accountBlocks[_account];
     accountBlocks[_account] = _accountBlocks.add(1);
   }
 
-  /**
-   * @dev Unblock account
-   * @param _account address Account ot unblock
-   */
   function unblockAccount(address _account) onlyOwner {
     var _accountBlocks = accountBlocks[_account];
     accountBlocks[_account] = _accountBlocks.sub(1);
   }
 
-  /**
-   * @dev Unblock account
-   * @param _account address Account ot unblock
-   */
   function isBlocked(address _account) constant returns (bool result) {
     return (accountBlocks[_account] > 0);
   }
@@ -48,11 +31,7 @@ contract BlockableToken is StandardToken, Ownable {
     return super.transferFrom(_from, _to, _value);
   }
 
-  /**
-   * @dev Throws if tokens of _from blocked
-   * @param _from address The address which you want to send tokens from
-   */
-  modifier onlyNotBlocked(address _from) {
+    modifier onlyNotBlocked(address _from) {
     if (accountBlocks[_from] > 0) {
       throw;
     }
